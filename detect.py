9262afb96adafb5ca9f274507874bc17f9dcfd87
Laurynas Butkus <laurynas.butkus@gmail.com>
2024-02-20 21:46:44 +0200

    Extract digitizer

--- detect.py
+++ detect.py
@@ -1,26 +1,8 @@
-from ultralytics import YOLO
+from lib.digitizer import Digitizer
 
 MODEL_FILE = 'models/yolov8-detect-20240220.pt'
 IMAGE_FILE = 'samples/watermeter1.jpg'
-CONFIDENCE = 0.5
-CLASSES = range(0, 9)
 
-model = YOLO(MODEL_FILE)
-results = model(IMAGE_FILE, conf=CONFIDENCE, classes=CLASSES)
-result = results[0]
+result = Digitizer(MODEL_FILE).run(IMAGE_FILE)
 
-def digitize(rows):
-    # sort by x position
-    rows = sorted(rows, key=lambda r: r[0])
-    reading = ''
-
-    for row in rows:
-        reading += str(int(row[-1]))
-
-    return reading
-
-data = result.boxes.data.cpu().tolist()
-
-print(digitize(data))
-#result.show()
-#print(result.tojson())
+print(result)
